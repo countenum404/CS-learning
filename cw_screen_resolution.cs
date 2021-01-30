594218c225a58249b8ce82c20600f2d484e78ad1
countenum404 <70239127+countenum404@users.noreply.github.com>
2021-01-30 17:00:24 +0300

    Add files via upload

---
+++ cw_screen_resolution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace str
+{
+    class str
+    {
+        static string Height(int width, string ratio)
+        {
+            string[] r = ratio.Split(":");
+            int result = width * Convert.ToInt32(r[1]) / Convert.ToInt32(r[0]);
+            return Convert.ToString(width) + "x" + Convert.ToString(result);
+        }
+        public static void Main(string[] args)
+        {
+            string output = Height(1024, "4:3");
+            Console.WriteLine(output);
+        }
+    }
+}
