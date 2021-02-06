b13e642f556c9710293cc4f344adfa17c2bebe25
countenum404 <70239127+countenum404@users.noreply.github.com>
2021-02-06 13:43:15 +0300

    Update ProgressBar.cs

--- ProgressBar.cs
+++ ProgressBar.cs
@@ -5,9 +5,8 @@ namespace Project
     class ProgressBar
     {
         
-        static void ProgressBar(int size = 10, int sec = 1)
+        static void ProgressBar(int size = 10, int mlsec = 500)
         {
-            int seconds = sec * 1000;
             string bar ="[";
             for (int i = 1; i < size; i++)
             {
@@ -19,7 +18,7 @@ namespace Project
             {
                 bar = bar.Remove(i, 1);
                 bar = bar.Insert(i, "|");
-                System.Threading.Thread.Sleep(seconds);
+                System.Threading.Thread.Sleep(mlsec);
                 Console.Write("\r" + bar);
             }
         }
